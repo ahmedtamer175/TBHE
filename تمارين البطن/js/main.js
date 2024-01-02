1dc03d41757f2456ee02579213407b8d685f1517
ahmedtamer175 <155467129+ahmedtamer175@users.noreply.github.com>
2024-01-02 20:03:41 +0300

    Add files via upload

---
+++ تمارين البطن/js/main.js
@@ -0,0 +1,80 @@
+
+var btn=document.getElementById("button"),
+    close=document.getElementById("close"),
+    div=document.getElementById("show");
+
+    button.onclick=function (){
+        div.style.display="block"
+
+        close.onclick=function (){
+            div.style.display="none"
+        }
+    }
+
+ 
+
+ let gobtn =  document.getElementById("go-btn1")
+
+ gobtn.classList.add("hide")
+ setTimeout(
+    function btnGo(){
+        gobtn.classList.remove("hide")
+        gobtn.classList.add("btn7646")
+    },62 * 1030)
+
+
+    var btn1=document.getElementById("button1"),
+    close1=document.getElementById("close1"),
+    div1=document.getElementById("show1");
+
+    btn1.onclick=function (){
+        div1.style.display="block"
+
+        close1.onclick=function (){
+            div1.style.display="none"
+        }
+    }
+
+
+    var btn2=document.getElementById("button2"),
+    close2=document.getElementById("close2"),
+    div2=document.getElementById("show2");
+
+    btn2.onclick=function (){
+        div2.style.display="block"
+
+        close2.onclick=function (){
+            div2.style.display="none"
+        }
+    }
+
+    var btn3=document.getElementById("button3"),
+    close3=document.getElementById("close3"),
+    div3=document.getElementById("show3");
+
+    btn3.onclick=function (){
+        div3.style.display="block"
+
+        close3.onclick=function (){
+            div3.style.display="none"
+        }
+    }
+
+  
+
+
+
+
+
+ 
+
+
+
+
+
+    
+        
+    
+    
+
+
